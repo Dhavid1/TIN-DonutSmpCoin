1f41488fb3084aef53a66c1774a4735e4417375b
Dhavid1 <130174798+Dhavid1@users.noreply.github.com>
2026-02-08 16:58:26 +0100

    Add files via upload

---
+++ TIN$/Info.js
@@ -0,0 +1,7 @@
+const OwnedTins = document.getElementById("OwnedTins");
+const CurrentOwners = document.getElementById("Owners");
+const TinStock = document.getElementById("TinStock");
+
+const ValueElement = document.getElementById("ValueElement");
+
+const ApiURL = "";
